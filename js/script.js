63351f70e11147a6636b45c7f233c8fe7e0154fe
lukaszplac123 <lukasz.plac@gmail.com>
2017-07-28 19:11:56 +0200

    Doctype declaration added

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ console.log('Hello, ' + name);
 /*Below code is just to show that block comments are possible within Javascript.
 It also shows that JS is good at Math*/
 var num1 = 123;
-var num2 = 322;
+var num2 = 321;
 alert("And the winning number is " + (num1+num2));
 
 //var num3;
